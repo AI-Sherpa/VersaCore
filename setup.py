69072dbd21ba1606c817e129c2cb4cac3e9ca6d9
AI-Sherpa <agtgtsmq@duck.com>
2024-07-07 18:33:20 +0800

    Updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VersaCore',
-    version='0.0.6',
+    version='0.0.7',
     description='VersaCore Library',
     author='Jansen Tang',
     author_email='jansen.tang@ai-sherpa.io',
