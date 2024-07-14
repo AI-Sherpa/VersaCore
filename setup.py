7b8b20cc22ad7c1b50f133e7e9aafb9357c278f7
AI-Sherpa <agtgtsmq@duck.com>
2024-07-14 21:01:00 +0800

    Updated README.md for webArticleSummariseQuery.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VersaCore',
-    version='0.0.8',
+    version='0.0.9',
     description='VersaCore Library',
     author='Jansen Tang',
     author_email='jansen.tang@ai-sherpa.io',
